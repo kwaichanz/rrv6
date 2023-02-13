38146aaf3d2a0feab2e2d35eb0e2f28d06d697c9
kwaichanz <alphezelphaz@gmail.com>
2023-02-13 14:54:28 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import { Link, NavLink, Route, Routes, useLocation, useRoutes } from 'react-router-dom'
+import { Home } from './components/Home'
+import { NotFound } from './components/NotFound'
+import { BookRoutes } from './BookRoutes'
+import "./styles.css"
+
+function App() {
+  const location = useLocation()
+  // let element = useRoutes([
+  //   {
+  //     path: "/",
+  //     element: <NavLayout />
+  //   },
+  //   {
+  //     path: "*",
+  //     element: <NotFound />
+  //   },
+  // ])
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" >Home</NavLink>
+          </li>
+          <li>
+            <NavLink end to="/books">Books</NavLink>
+          </li>
+        </ul>
+      </nav>
+      {location.state}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hi" element={<h3>Hi hi hi</h3>} />
+        <Route path="/books/*" element={<BookRoutes />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BookList.jsx
@@ -0,0 +1,5 @@
+import { Link } from "react-router-dom"
+
+export function BookList() {
+    return  <h1>BookList</h1>
+}
